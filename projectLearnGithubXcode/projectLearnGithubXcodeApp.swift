c28ff86a70fcf02c4d93e16ebc824dcb6190f1b7
costa-rica <nrodrig1@gmail.com>
2022-01-19 13:48:07 +0100

    Initial Commit

---
+++ projectLearnGithubXcode/projectLearnGithubXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectLearnGithubXcodeApp.swift
+//  projectLearnGithubXcode
+//
+//  Created by Nick on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct projectLearnGithubXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
